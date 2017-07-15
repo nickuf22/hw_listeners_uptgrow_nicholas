bd5072713ee5433fc4a4ab84c40a8cfe0167ee7e
Nicholas Uptgrow <nicholasuptgrow@gmail.com>
2017-07-14 22:23:28 -0400

    Add increment to listener

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+var numba = 0;
 var heading1 = document.getElementsByTagName('h1')[0];
 
 heading1.addEventListener('click', myFunction);
 
 function myFunction() {
     var paragraph = document.createElement('p');
-    paragraph.innerHTML = 'This is click number: ';
+    paragraph.innerHTML = 'This is click number: ' + ++numba;
     document.body.appendChild(paragraph);
 }
