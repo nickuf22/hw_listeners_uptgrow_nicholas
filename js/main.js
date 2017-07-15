7c9c714ef3010c03107460dfbfe3851d5832c968
Nicholas Uptgrow <nicholasuptgrow@gmail.com>
2017-07-15 06:49:48 -0400

    Fix event listeners

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 var image = document.getElementById('figure');
 
+// Shows when the mouse hover over the figure
 function myFunction() {
-    document.getElementById('testing').innerHTML += 'Hover over!<br>';
+    document.getElementById('testing').innerHTML = 'Hover over<br>';
 }
 
+// Shows when the mouse moves from over the figure
 function mySecondFunction() {
-    document.getElementById('testing').innerHTML += 'Hover out!<br>';
+    document.getElementById('testing').innerHTML = 'Hover out<br>';
 }
 
+// Notifies users upon clicking the image
 function myThirdFunction() {
-    document.getElementById('testing').innerHTML += 'Image is not hyerlinked<br>';
+    document.getElementById('testing').innerHTML = 'Image is not hyerlinked<br>';
 }
 
 image.addEventListener('mouseover', myFunction);
